bdcf55ab3f3c8bee101eceb6115ca53192149f05
m-seveigny <m-seveigny@JLK307-2501E>
2025-10-30 12:24:14 -0400

    Add project files.

---
+++ Lab9_1_Array/Part 1.cpp
@@ -0,0 +1,13 @@
+/*
+Part 1.cpp
+Michael Seveigny
+10/30/2025
+Description: Write a program that initializes an array with ten 4-digit random integers (obtained from the rand() function)
+and then prints five lines of output, containing:
+• Every element in order of appearance in the array.
+• Every element at an even index.
+• Every even element.
+• All elements in reverse order.
+• Only the first and last element.
+*/
+
